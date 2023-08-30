afde44823399fd711f98768903c87e4cf395a634
Donizete Junior <donizete.junior>
2023-08-30 11:28:04 -0300

    Configuration  Router

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEditMedicamentoComponent } from './components/add-edit-medicamento/add-edit-medicamento.component';
+import { ListMedicamentosComponent } from './components/list-medicamentos/list-medicamentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListMedicamentosComponent },
+  { path: 'add-medicamento', component: AddEditMedicamentoComponent },
+  { path: 'editar-medicamento/:id', component: AddEditMedicamentoComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/components/list-medicamentos/list-medicamentos.component.ts
+++ front-end/src/app/components/list-medicamentos/list-medicamentos.component.ts
@@ -8,9 +8,9 @@ import { Medicamento } from 'src/app/interfaces/medicamento';
 })
 export class ListMedicamentosComponent implements OnInit {
   listMedicamentos: Medicamento[] = [
-    { nome_medicamento: 'Dipirona', descricao: 'Uso oral', quantidade: 1500, laboratorio: 'BellPharma', forma_farmaceutica: 'Líquida', estoque: 0 },
-    { nome_medicamento: 'Dorflex', descricao: 'Uso oral', quantidade: 580, laboratorio: 'BellPharma', forma_farmaceutica: 'Comprimido', estoque: 3 },
-    { nome_medicamento: 'Halobex', descricao: 'Passar na ferida', quantidade: 250, laboratorio: 'BellPharma', forma_farmaceutica: 'pasta', estoque: 0 },
+    { id: 1, nome_medicamento: 'Dipirona', descricao: 'Uso oral', quantidade: 1500, laboratorio: 'BellPharma', forma_farmaceutica: 'Líquida', estoque: 0 },
+    { id: 2, nome_medicamento: 'Dorflex', descricao: 'Uso oral', quantidade: 580, laboratorio: 'BellPharma', forma_farmaceutica: 'Comprimido', estoque: 3 },
+    { id: 3, nome_medicamento: 'Halobex', descricao: 'Passar na ferida', quantidade: 250, laboratorio: 'BellPharma', forma_farmaceutica: 'pasta', estoque: 0 },
 
   ]
   constructor() {
