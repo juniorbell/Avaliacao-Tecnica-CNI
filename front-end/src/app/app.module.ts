1b2aabc47712e7bb9aa695a1c167316294fe32bc
Donizete Junior <donizete.junior>
2023-08-30 13:38:36 -0300

    Feat: Validation addMedicamento

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ListMedicamentosComponent } from './components/list-medicamentos/list-medicamentos.component';
+//Módulos da aplicação
+import { ReactiveFormsModule } from '@angular/forms';
+//Components da aplicação
 import { AddEditMedicamentoComponent } from './components/add-edit-medicamento/add-edit-medicamento.component';
+import { ListMedicamentosComponent } from './components/list-medicamentos/list-medicamentos.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { AddEditMedicamentoComponent } from './components/add-edit-medicamento/a
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/components/add-edit-medicamento/add-edit-medicamento.component.ts
+++ front-end/src/app/components/add-edit-medicamento/add-edit-medicamento.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Medicamento } from 'src/app/interfaces/medicamento';
 
 @Component({
   selector: 'app-add-edit-medicamento',
   templateUrl: './add-edit-medicamento.component.html',
   styleUrls: ['./add-edit-medicamento.component.css']
 })
-export class AddEditMedicamentoComponent {
+export class AddEditMedicamentoComponent implements OnInit {
+  formMedicamento: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.formMedicamento = this.fb.group({
+      nome_medicamento: ['', Validators.required],
+      descricao: ['', Validators.required],
+      quantidade: ['', Validators.required],
+      laboratorio: ['', Validators.required],
+      forma_farmaceutica: ['', Validators.required],
+      estoque: ['', Validators.required],
+
+    })
+
+  }
+
+  ngOnInit(): void {
+
+  };
+  addMedicamento() {
+    const medicamento: Medicamento = {
+      nome_medicamento: this.formMedicamento.value.nome_medicamento,
+      descricao: this.formMedicamento.value.descricao,
+      quantidade: this.formMedicamento.value.quantidade,
+      laboratorio: this.formMedicamento.value.laboratorio,
+      forma_farmaceutica: this.formMedicamento.value.forma_farmaceutica,
+      estoque: this.formMedicamento.value.estoque,
+    }
+    console.log(medicamento);
+  };
 }
